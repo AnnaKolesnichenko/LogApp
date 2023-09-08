47dd4f6cc7400ce57ac3c9ef18a90146affd3eab
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-09-08 16:29:50 +0300

    f

--- src/screens/RegistrationScreen.jsx
+++ src/screens/RegistrationScreen.jsx
@@ -24,10 +24,6 @@ const RegistrationScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [login, setLogin] = useState("");
-  const [passVisible, setPassVisible] = useState(true);
-  // const [loginFocused, setLoginFocused] = useState(false);
-  // const [emailFocused, setEmailFocused] = useState(false);
-  const [passFocused, setPassFocused] = useState(false);
 
   const getInputData = () => {
     console.log({ email, login, password });
@@ -148,31 +144,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 15,
   },
-  input: {
-    marginTop: 15,
-    padding: 10,
-    width: "100%",
-    height: 50,
-    backgroundColor: "#f9f4f4",
-    borderColor: "#ede8e8",
-    borderWidth: 1,
-    borderRadius: 10,
-  },
-  showPassInput: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  toggleButton: {
-    paddingHorizontal: 15,
-    paddingTop: 10,
-    position: "absolute",
-    right: 0,
-  },
-  toggleButtonText: {
-    fontSize: 12,
-    fontWeight: "600",
-    color: "#3d4044",
-  },
+
   outerContainer: {
     width: "100%",
     alignItems: "center",
